47bcbf4fcd9d788ed6459f4ccedf1c6801f7b2a4
avetisminasyan <avominasyan71@gmail.com>
2023-02-06 21:27:05 +0400

    first

---
+++ bash1.sh
@@ -0,0 +1,139 @@
+#!/usr/bin/bash
+#echo "whis is bash 1 scrypt"
+#a="first vars"
+#echo $a
+#pwd ; whoami
+
+# our comment is herer 
+
+#echo "the curent directory"
+#pwd
+#echo "the user logges in is"
+#whoami 
+#echo avominasyan
+#echo "home for the current user $Home"
+#echo "price is \$100"
+
+#testing variables
+#grade=5
+#person="Adam"
+#echo "$person is a good boy,he is in grade $grade"
+#pwd_d=`pwd`
+#echo $pwd_d
+#my_dir=$(pwd)
+#echo $my_dir
+
+#var1=$((5+5))
+#echo $var1
+#var2=$(($var1+15))
+#echo $var2
+#if pwd 
+#then
+#	echo "it works"
+#fi
+
+#user="root"
+#if grep $user /etc/passwd
+#then
+#	echo "THe user $user exists"
+#fi
+
+#user="asdasdasd"
+# if grep $user /etc/passwd
+# then
+#	 echo "the user $user exists"
+#else
+#	echo "the user $user doeesnt exist"
+#fi
+
+#user="asdsa"
+#if grep $user /etc/passwd
+#then
+#	echo "The user $user exists"
+#elif ls /home
+#then
+#	echo "The user doesnt exist"
+#fi
+
+var1=5
+var2=7
+#if [ $var1 -eq 5 ]
+#then
+#	echo " var1==5"
+#else
+#	echo "var1!=5"
+#fi
+
+
+#if [ $var1 -ge 6 ]
+#then
+#	echo "var1>6"
+#else
+#	echo "var1 pqor 6"
+#fi
+
+#if [ $var1 -gt 3 ];
+#then
+#	echo "var1 greater than 3"
+#else
+#	echo "var1 poqre 3"
+#fi
+
+#if [ $var1 -le 10 ];
+#then
+#	echo "var1 poqr havasare 10 "
+#else
+#	echo "var1 mec e 10"
+#fi
+
+#if [ $var1 -lt 15 ];
+#then
+#	echo "var1 poqre 15"
+#else
+#	echo "var2 mece 15 "
+#fi
+
+#if [ $var1 -ne 15 ];
+#then
+#	echo "var1!=15"
+#else
+#	echo "var1==15"
+#fi
+
+#str_1="hello"
+#str_2="world"
+#if [ $str_1=$str_2 ]
+#then
+#	echo "str_1==str_2"
+#else
+#	echo "str1!=str2"
+#fi
+
+#if [ $str_1!="adsfsdf" ]
+#then
+#	echo "str_1!=assad"
+#else
+#	echo "str_2==sadsad"
+#fi
+
+#if [ $str_1>$str_2 ];
+#then
+#	echo "str_1>str_2"
+#else
+#	echo "str_1<str_2"
+#fi
+
+#if [ -n $str_1 ];
+#then
+#	echo "str_1 len mece 1"
+#else
+#	echo "str_2 len poqre 1-ic"
+#fi
+
+#if [ -z $str_1  ]
+#then
+#	echo "asdadasd"
+#else
+#	echo "asdasdasd"
+#fi
+echo "hello world"
